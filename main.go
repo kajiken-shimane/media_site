74995acdd81de7393f8922d070d00e16c2d8063e
Your Name <5161005@st.hsc.ac.jp>
2019-12-14 17:26:33 +0900

    dev作成　ログイン・サインアップ機能の追加など

--- main.go
+++ main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/go-media-site/routes"
 )
 
 func main() {
 	router := gin.Default()
-	router.LoadHTMLGlob("templates/*.html")
+	router.LoadHTMLGlob("views/*.html")
+	router.Static("/assets", "./assets")
 
-	router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(200, "index.html", gin.H{})
-	})
+	user := router.Group("/user")
+	{
+		user.POST("/signup", routes.UserSignUp)
+		user.POST("/login", routes.UserLogIn)
+	}
+
+	router.GET("/", routes.Home)
+	router.GET("/login", routes.LogIn)
+	router.GET("/signup", routes.SignUp)
+	router.NoRoute(routes.NoRoute)
+
+	// router.GET("/", func(ctx *gin.Context) {
+	// 	ctx.HTML(200, "index.html", gin.H{})
+	// })
 
 	router.Run()
 }
