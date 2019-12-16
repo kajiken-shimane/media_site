0f4dfd0546b2d571208bd27045d52809c5f9abf4
Your Name <5161005@st.hsc.ac.jp>
2019-12-16 19:01:16 +0900

    パスワードのハッシュ化

--- routes/user_routes.go
+++ routes/user_routes.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"github.com/go-media-site/config"
+
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,10 +14,21 @@ func UserSignUp(ctx *gin.Context) {
 	email := ctx.PostForm("emailaddress")
 	password := ctx.PostForm("password")
 	passwordConf := ctx.PostForm("passwordconfirmation")
-	println("username: " + username)
-	println("email: " + email)
-	println("password: " + password)
-	println("passwordConf: " + passwordConf)
+
+	if password != passwordConf {
+		println("Error: password and passwordConf not match")
+		ctx.Redirect(http.StatusSeeOther, "//localhost:8080/")
+		return
+	}
+
+	db := config.DummyDB()
+	if err := db.SaveUser(username, email, password); err != nil {
+		println("Error: " + err.Error())
+	} else {
+
+		println("Signup success!!")
+
+	}
 
 	ctx.Redirect(http.StatusSeeOther, "//localhost:8080/")
 }
@@ -24,8 +37,18 @@ func UserLogIn(ctx *gin.Context) {
 	println("post/login")
 	username := ctx.PostForm("username")
 	password := ctx.PostForm("password")
-	println("username: " + username)
-	println("password: " + password)
+
+	db := config.DummyDB()
+	user, err := db.GetUser(username, password)
+	if err != nil {
+		println("Error: " + err.Error())
+	} else {
+		println("Authentication Success!!")
+		println("  username: " + user.Username)
+		println("  email: " + user.Email)
+		println("  password: " + user.Password)
+		user.Authenticate()
+	}
 
 	ctx.Redirect(http.StatusSeeOther, "//localhost:8080/")
 }
